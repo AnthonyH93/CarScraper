1e33c25dc73a2e7d24ac413bd05597da1cd43304
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2021-02-07 20:59:42 -0500

    Depper scraping to look for individual cars

--- scripts/car_scraping.py
+++ scripts/car_scraping.py
@@ -5,16 +5,26 @@
 import requests
 from bs4 import BeautifulSoup
 from .models.car import Car
+from random import randrange
+import time
+
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
 
 # Class to describe the car scraper
 class CarScraper:
     def __init__(self, manufacturer):
-        self.base_url = 'https://en.wikipedia.org/wiki/'
+        self.base_url = 'https://en.wikipedia.org'
         self.manufacturer = manufacturer
         self.start_url = ''
     
     def setup_scraping(self):
-        self.start_url = self.base_url + self.manufacturer
+        self.start_url = self.base_url + '/wiki/' + self.manufacturer
         # Jaguar is a special case
         if (self.manufacturer == 'Jaguar'):
             self.start_url = self.start_url + '_Cars'
@@ -22,9 +32,9 @@ class CarScraper:
 
     def perform_scraping(self):
         # Get the first data from the start url
-        start_page = requests.get(self.start_url)
+        start_page = requests.get(self.start_url, headers)
         if (start_page.status_code > 299 or start_page.status_code < 200):
-            print('Invalid request for ' +self.start_url + ' status code was ' + start_page.status_code)
+            print('Invalid request for ' + self.start_url + ' status code was ' + start_page.status_code)
             exit
         
         soup = BeautifulSoup(start_page.content, 'html.parser')
@@ -43,15 +53,54 @@ class CarScraper:
                 else:
                     links_to_lists.append(link.get('href'))
         
+        # Decide next URL to scrape
+        next_url = ''
         if len(links_to_lists) == 0:
             # Need to find a different link to follow
-            print(0)
+            next_url = self.start_url
         elif len(links_to_lists) > 1:
             # Need to pick one of the links to follow
             print(len(links_to_lists))
-            print(links_to_lists)
+            random_selection = randrange(len(links_to_lists))
+            next_url = self.base_url + links_to_lists[random_selection]
         else:
             # Should have a link to a list of vehicles by the given manufacturer
             print(1)
             print(links_to_lists[0])
+            next_url = self.base_url + links_to_lists[0]
+        
+        next_page = requests.get(next_url, headers)
+        if (next_page.status_code > 299 or next_page.status_code < 200):
+            print('Invalid request for ' + next_url + ' status code was ' + next_page.status_code)
+            exit
+        
+        soup = BeautifulSoup(next_page.content, 'html.parser')
+        # Get all links which have a title
+        all_valid_links = soup.find_all(lambda tag:tag.name == 'a' and 'title' in tag.attrs)
+
+        # Discard all links without the car manufacturer in the title
+        possible_car_links = []
+        for link in all_valid_links:
+            if (self.manufacturer not in link.get('title')):
+                continue
+            else:
+                possible_car_links.append(self.base_url + link.get('href'))
+
+        print(len(possible_car_links))
+        # Explore each potential car link and extract information if a car is found
+        link_counter = 0
+        for link in possible_car_links:
+            # Delay to avoid overloading the server
+            time.sleep(0.1)
+            current_page = requests.get(possible_car_links[link_counter], headers)
+            link_counter += 1
+
+            # Skip invalid pages (if any exist)
+            if (next_page.status_code > 299 or next_page.status_code < 200):
+                continue
+            # Determine if page contains a car and act accordingly
+            else:
+                soup = BeautifulSoup(current_page.content, 'html.parser')
+                all_tables = soup.find_all('table', class_='infobox hproduct')
+                print(all_tables)
 
